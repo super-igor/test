49a02a017a968a842882b860dd69b56c1d229bf8
super-igor <71583192+super-igor@users.noreply.github.com>
2021-01-17 12:58:51 +0400

    Add files via upload

---
+++ game_joke.py
@@ -0,0 +1,22 @@
+import pygame as pg
+
+class Window:
+    w1 = 1000
+    w2 = 600
+class Button:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def ispressed(self):
+        pass
+    def run(self):
+        pass
+    def set_text(self):
+        pass
+    def set_color(self):
+        pass
+    def set_size(self):
+        pass
+    
+screen = pg.display.set_mode((Window.w1, Window.w2))
